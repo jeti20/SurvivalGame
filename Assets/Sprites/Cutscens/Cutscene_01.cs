d7bbe49b1727a72939d0150cd374bec45769ec0d
Dehydrogenase <michalrodak98@gmail.com>
2024-12-04 16:57:27 +0100

    Cutscene_01 added  + sound  + beggining axe

---
+++ Assets/Sprites/Cutscens/Cutscene_01.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Cutscene_01 : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    //public GameObject TextBox;
+    public AudioSource line1;
+    //public AudioSource line2;
+
+    void Start()
+    {
+        
+        player.GetComponent<PlayerController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1.5f);
+        //TextBox.GetComponent<TextMeshProUGUI>().text = "... wherer am I?";
+        line1.Play();
+        yield return new WaitForSeconds(2); 
+        //TextBox.GetComponent<TextMeshProUGUI>().text = ""; 
+        yield return new WaitForSeconds(0.5f);
+        //GetComponent<TextMeshProUGUI>().text = "I need to get out of here.";
+        //line2.Play();
+        yield return new WaitForSeconds(4);
+        //TextBox.GetComponent<TextMeshProUGUI>().text = "";
+        player.GetComponent<PlayerController>().enabled = true;
+
+    }
+}
