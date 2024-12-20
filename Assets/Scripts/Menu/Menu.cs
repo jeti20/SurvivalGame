68fb3dbb7b477674f8f20167ef277c2a5eff5fa9
Dehydrogenase <michalrodak98@gmail.com>
2024-12-20 11:49:08 +0100

    Menu fix after new scene loaded

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -5,17 +5,15 @@ public class Menu : MonoBehaviour
 {
     private void Awake()
     {
-        // W³¹cz widocznoœæ kursora i odblokuj go
         Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
-    // Called when we click the "Play" button.
     public void OnPlayButton()
     {
         SceneManager.LoadScene(1);
     }
 
-    // Called when we click the "Quit" button.
     public void OnQuitButton()
     {
         Application.Quit();
